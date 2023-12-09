77310e37497a7ae5b649a8749f639391abf43230
Therealaly <aadley85@gmail.com>
2023-12-09 18:58:40 +0700

    art detail connection

--- app/Http/Controllers/ArtController.php
+++ app/Http/Controllers/ArtController.php
@@ -9,7 +9,7 @@ class ArtController extends Controller
 {
     public function show($id)
     {
-        $response = Http::get("http://localhost:8000/api/v1/records/arts/{$id}", ['timeout' => 60]);
+        $response = Http::get("http://127.0.0.1:8000/api/v1/records/arts/{$id}", ['timeout' => 60]);
         $art = $response->json();
 
         return view('pages.artDetail', compact('art'));
