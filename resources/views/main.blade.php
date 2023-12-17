f0fe05972c2b328a417b5e2597a64d288225de4a
falahrafi <falahabdurrafi@gmail.com>
2023-12-17 18:20:05 +0700

    Add Meta for Dicoding Email Verification

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,6 +4,8 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      {{-- Dicoding Email Verification --}}
+      <meta name="dicoding:email" content="falahabdurrafi@gmail.com">
       <title>GalseId | {{ $title }}</title>
       {{-- Favicon --}}
       <link rel="icon" sizes="32x32" href="./favicon-dark.png" media="(prefers-color-scheme: dark)" />
